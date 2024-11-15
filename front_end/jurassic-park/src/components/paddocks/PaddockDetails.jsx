07becbe3f806129b555d6ab021ace8526d9d6585
dsousa034 <150059798+dsousa034@users.noreply.github.com>
2024-11-15 22:06:15 +0100

    matriz

--- front_end/jurassic-park/src/components/paddocks/PaddockDetails.jsx
+++ front_end/jurassic-park/src/components/paddocks/PaddockDetails.jsx
@@ -7,16 +7,32 @@ const PaddockDetails = (props) => {
     props.onPaddockDelete(props.paddock.id)
   }
 
+  const renderMatrix = () => {
+    const cells = [];
+    for (let i = 0; i < 100; i++) {
+      cells.push(<div key={i} className="matrix-cell"></div>);
+    }
+    return cells;
+  }
+
   return(
-    <div className="paddock-details-container">
-      <div className="component" id="paddock-details">
-        <h3>Paddock Details</h3>
-        <Paddock paddock={props.paddock}/>
-        <div className="buttons">
-          <button onClick={handlePaddockDelete}>Remove Paddock</button>
+      <div>
+        <div className="paddock-details-container">
+          <div className="component" id="paddock-details">
+            <h3>Paddock Details</h3>
+            <Paddock paddock={props.paddock}/>
+            <div className="buttons">
+              <button onClick={handlePaddockDelete}>Remove Paddock</button>
+            </div>
+          </div>
+        </div>
+        <div className="matrix-container">
+          <div className="matrix">
+            {renderMatrix()}
+          </div>
         </div>
       </div>
-    </div>
   )
 }
-export default PaddockDetails
+
+export default PaddockDetails;
